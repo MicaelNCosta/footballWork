d479de0b233f841f771ea4dd1d4d255a3d475d0e
MicaelNCosta <micael.nc01@gmail.com>
2023-07-25 22:23:31 -0300

    botão para ir ao formulario do frmPri

--- Projeto1/Projeto1/frmPrincipal.Designer.cs
+++ Projeto1/Projeto1/frmPrincipal.Designer.cs
@@ -29,13 +29,25 @@ namespace projeto1
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1275, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Formulario";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // FrmPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1461, 822);
+            this.Controls.Add(this.button1);
             this.Name = "FrmPrincipal";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.FrmPrincipal_Load);
@@ -44,6 +56,8 @@ namespace projeto1
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
