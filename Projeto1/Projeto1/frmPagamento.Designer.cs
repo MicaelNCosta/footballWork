de6510d8bea31f22217c342df353f3999e8b9a4a
jefytalmeidarios15 <jefytalmeidarios@gmail.com>
2023-07-28 22:12:14 -0300

    terminei a tela de pagamento e codifiquei

--- Projeto1/Projeto1/frmPagamento.Designer.cs
+++ Projeto1/Projeto1/frmPagamento.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.mskCnpj = new System.Windows.Forms.MaskedTextBox();
             this.txtTitular = new System.Windows.Forms.TextBox();
-            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.mskCvc = new System.Windows.Forms.MaskedTextBox();
             this.dtpValidade = new System.Windows.Forms.DateTimePicker();
             this.label1 = new System.Windows.Forms.Label();
             this.lblCnpj = new System.Windows.Forms.Label();
@@ -41,14 +41,19 @@
             this.txtSenha = new System.Windows.Forms.TextBox();
             this.btnProsseguir = new System.Windows.Forms.Button();
             this.gbSelecione = new System.Windows.Forms.GroupBox();
-            this.rbCpf = new System.Windows.Forms.RadioButton();
             this.rbCnpj = new System.Windows.Forms.RadioButton();
+            this.rbCpf = new System.Windows.Forms.RadioButton();
             this.lblCpf = new System.Windows.Forms.Label();
             this.mskCpf = new System.Windows.Forms.MaskedTextBox();
             this.mskCartao = new System.Windows.Forms.MaskedTextBox();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label2 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pbOlho1 = new System.Windows.Forms.PictureBox();
             this.gbSelecione.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbOlho1)).BeginInit();
             this.SuspendLayout();
             // 
             // mskCnpj
@@ -60,32 +65,34 @@
             this.mskCnpj.TabIndex = 0;
             this.mskCnpj.UseWaitCursor = true;
             this.mskCnpj.Visible = false;
+            this.mskCnpj.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.mskCnpj_MaskInputRejected);
             // 
             // txtTitular
             // 
-            this.txtTitular.Location = new System.Drawing.Point(632, 24);
+            this.txtTitular.Location = new System.Drawing.Point(608, 24);
             this.txtTitular.Name = "txtTitular";
-            this.txtTitular.Size = new System.Drawing.Size(100, 20);
+            this.txtTitular.Size = new System.Drawing.Size(124, 20);
             this.txtTitular.TabIndex = 1;
             this.txtTitular.UseWaitCursor = true;
             this.txtTitular.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // maskedTextBox2
+            // mskCvc
             // 
-            this.maskedTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.maskedTextBox2.Location = new System.Drawing.Point(147, 243);
-            this.maskedTextBox2.Mask = "0/0/0";
-            this.maskedTextBox2.Name = "maskedTextBox2";
-            this.maskedTextBox2.Size = new System.Drawing.Size(51, 29);
-            this.maskedTextBox2.TabIndex = 2;
-            this.maskedTextBox2.UseWaitCursor = true;
+            this.mskCvc.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mskCvc.Location = new System.Drawing.Point(147, 243);
+            this.mskCvc.Mask = "0/0/0";
+            this.mskCvc.Name = "mskCvc";
+            this.mskCvc.Size = new System.Drawing.Size(51, 29);
+            this.mskCvc.TabIndex = 2;
+            this.mskCvc.UseWaitCursor = true;
+            this.mskCvc.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.mskCvc_MaskInputRejected);
             // 
             // dtpValidade
             // 
             this.dtpValidade.CustomFormat = "MM/yy";
             this.dtpValidade.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.dtpValidade.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dtpValidade.Location = new System.Drawing.Point(162, 320);
+            this.dtpValidade.Location = new System.Drawing.Point(162, 313);
             this.dtpValidade.Name = "dtpValidade";
             this.dtpValidade.Size = new System.Drawing.Size(83, 29);
             this.dtpValidade.TabIndex = 3;
@@ -140,7 +147,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(323, 92);
+            this.label5.Location = new System.Drawing.Point(385, 98);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(155, 20);
             this.label5.TabIndex = 9;
@@ -151,7 +158,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(408, 248);
+            this.label6.Location = new System.Drawing.Point(408, 161);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(70, 24);
             this.label6.TabIndex = 10;
@@ -161,9 +168,10 @@
             // txtSenha
             // 
             this.txtSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtSenha.Location = new System.Drawing.Point(532, 248);
+            this.txtSenha.Location = new System.Drawing.Point(608, 156);
             this.txtSenha.Name = "txtSenha";
-            this.txtSenha.Size = new System.Drawing.Size(100, 29);
+            this.txtSenha.PasswordChar = '*';
+            this.txtSenha.Size = new System.Drawing.Size(124, 29);
             this.txtSenha.TabIndex = 11;
             this.txtSenha.UseWaitCursor = true;
             this.txtSenha.TextChanged += new System.EventHandler(this.txtSenha_TextChanged);
@@ -172,7 +180,7 @@
             // 
             this.btnProsseguir.BackColor = System.Drawing.Color.Red;
             this.btnProsseguir.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnProsseguir.Location = new System.Drawing.Point(632, 378);
+            this.btnProsseguir.Location = new System.Drawing.Point(523, 299);
             this.btnProsseguir.Name = "btnProsseguir";
             this.btnProsseguir.Size = new System.Drawing.Size(135, 60);
             this.btnProsseguir.TabIndex = 12;
@@ -195,20 +203,6 @@
             this.gbSelecione.Text = "Selecione";
             this.gbSelecione.UseWaitCursor = true;
             // 
-            // rbCpf
-            // 
-            this.rbCpf.AutoSize = true;
-            this.rbCpf.ForeColor = System.Drawing.Color.Black;
-            this.rbCpf.Location = new System.Drawing.Point(26, 20);
-            this.rbCpf.Name = "rbCpf";
-            this.rbCpf.Size = new System.Drawing.Size(51, 19);
-            this.rbCpf.TabIndex = 0;
-            this.rbCpf.TabStop = true;
-            this.rbCpf.Text = "CPF";
-            this.rbCpf.UseVisualStyleBackColor = true;
-            this.rbCpf.UseWaitCursor = true;
-            this.rbCpf.CheckedChanged += new System.EventHandler(this.rbCpf_CheckedChanged);
-            // 
             // rbCnpj
             // 
             this.rbCnpj.AutoSize = true;
@@ -222,6 +216,20 @@
             this.rbCnpj.UseVisualStyleBackColor = true;
             this.rbCnpj.UseWaitCursor = true;
             this.rbCnpj.CheckedChanged += new System.EventHandler(this.rbCnpj_CheckedChanged);
+            // 
+            // rbCpf
+            // 
+            this.rbCpf.AutoSize = true;
+            this.rbCpf.ForeColor = System.Drawing.Color.Black;
+            this.rbCpf.Location = new System.Drawing.Point(16, 20);
+            this.rbCpf.Name = "rbCpf";
+            this.rbCpf.Size = new System.Drawing.Size(51, 19);
+            this.rbCpf.TabIndex = 0;
+            this.rbCpf.TabStop = true;
+            this.rbCpf.Text = "CPF";
+            this.rbCpf.UseVisualStyleBackColor = true;
+            this.rbCpf.UseWaitCursor = true;
+            this.rbCpf.CheckedChanged += new System.EventHandler(this.rbCpf_CheckedChanged);
             // 
             // lblCpf
             // 
@@ -237,13 +245,14 @@
             // 
             // mskCpf
             // 
-            this.mskCpf.Location = new System.Drawing.Point(104, 167);
+            this.mskCpf.Location = new System.Drawing.Point(104, 166);
             this.mskCpf.Mask = "000.000.000-00";
             this.mskCpf.Name = "mskCpf";
             this.mskCpf.Size = new System.Drawing.Size(94, 20);
             this.mskCpf.TabIndex = 15;
             this.mskCpf.UseWaitCursor = true;
             this.mskCpf.Visible = false;
+            this.mskCpf.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.mskCpf_MaskInputRejected);
             // 
             // mskCartao
             // 
@@ -257,26 +266,73 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.Red;
-            this.panel1.Location = new System.Drawing.Point(295, 0);
+            this.panel1.Location = new System.Drawing.Point(295, -4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(28, 450);
+            this.panel1.Size = new System.Drawing.Size(22, 454);
             this.panel1.TabIndex = 17;
+            this.panel1.UseWaitCursor = true;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Swis721 BlkEx BT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(8, 9);
+            this.label2.Font = new System.Drawing.Font("Swis721 BlkEx BT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Brown;
+            this.label2.Location = new System.Drawing.Point(12, 44);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(255, 19);
+            this.label2.Size = new System.Drawing.Size(207, 16);
             this.label2.TabIndex = 18;
             this.label2.Text = "Preencha seus dados";
+            this.label2.UseWaitCursor = true;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Swis721 BlkEx BT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(9, 5);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(277, 39);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Parabens você seleceionou nosso plano premiun";
+            this.label7.UseWaitCursor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(569, 311);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 13);
+            this.label8.TabIndex = 20;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(712, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 35);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "Limpar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pbOlho1
+            // 
+            this.pbOlho1.Image = global::projeto1.Properties.Resources.olho__2__certo;
+            this.pbOlho1.Location = new System.Drawing.Point(750, 156);
+            this.pbOlho1.Name = "pbOlho1";
+            this.pbOlho1.Size = new System.Drawing.Size(27, 31);
+            this.pbOlho1.TabIndex = 22;
+            this.pbOlho1.TabStop = false;
+            this.pbOlho1.Click += new System.EventHandler(this.pbOlho1_Click);
             // 
             // frmPagamento
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pbOlho1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.mskCartao);
@@ -292,7 +348,7 @@
             this.Controls.Add(this.lblCnpj);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dtpValidade);
-            this.Controls.Add(this.maskedTextBox2);
+            this.Controls.Add(this.mskCvc);
             this.Controls.Add(this.txtTitular);
             this.Controls.Add(this.mskCnpj);
             this.Name = "frmPagamento";
@@ -301,6 +357,7 @@
             this.Load += new System.EventHandler(this.frmPagamento_Load);
             this.gbSelecione.ResumeLayout(false);
             this.gbSelecione.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbOlho1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -310,7 +367,7 @@
 
         private System.Windows.Forms.MaskedTextBox mskCnpj;
         private System.Windows.Forms.TextBox txtTitular;
-        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.MaskedTextBox mskCvc;
         private System.Windows.Forms.DateTimePicker dtpValidade;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label lblCnpj;
@@ -328,5 +385,9 @@
         private System.Windows.Forms.MaskedTextBox mskCartao;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pbOlho1;
     }
 }
--- Projeto1/Projeto1/frmPagamento.cs
+++ Projeto1/Projeto1/frmPagamento.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -19,7 +20,6 @@ namespace projeto1
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-
         }
 
         private void rbCpf_CheckedChanged(object sender, EventArgs e)
@@ -70,7 +70,141 @@ namespace projeto1
 
         private void btnProsseguir_Click(object sender, EventArgs e)
         {
+            char[] cleanchar = { '.', '/', '-', ' ' };
+            string nome = txtTitular.Text;
+            string confrimar = btnProsseguir.Text;
+            string senha = txtSenha.Text;
+            string  CPF = mskCpf.Text.Trim(cleanchar);
+            string CNPJ = mskCnpj.Text.Trim(cleanchar);
+            string CVC = mskCvc.Text.Trim(cleanchar);
+            string Cartao = mskCartao.Text.Trim();
+            if (txtTitular.Text == "")
+            {
+                txtTitular.Focus();
+
+
+                MessageBox.Show("digite o nome do titular do cartão");
+            }
+
+            if (txtSenha.Text != "jef123")                   
+            {
+                        txtSenha.Focus();
+                        MessageBox.Show("Senha incorreta");
+                
+            }
+            if (txtSenha.Text == "jef123")
+            {
+                MessageBox.Show("Parabens você concluiu o pagamento , agradecemos a preferencia,atenciosamente footballWork.");
+            }
+            if (rbCpf.Checked && CPF.Length<11)
+                {
+                    mskCpf.Focus();
+                    MessageBox.Show("preencha o CPF com 11 caracteres");
+                    mskCpf.BackColor = Color.Red;
+                }
             
+            if (rbCnpj.Checked && CNPJ.Length<14)
+            {
+                mskCnpj.Focus();
+                MessageBox.Show("preencha o CNPJ com 14 caracteres");
+                mskCnpj.BackColor = Color.Red;
+            }
+
+            if (CVC.Length<3)
+            { 
+                    mskCvc.Focus();
+                    MessageBox.Show("preencha CVC com 3 caracteres");
+                    mskCvc.BackColor = Color.Red;
+                    
+            }
+            if (Cartao.Length<16)
+            {
+                mskCartao.Focus();
+                MessageBox.Show("preencha o numero do cartãp com 16 caracteres");
+                mskCartao.BackColor = Color.Red;
+            }
+            MessageBox.Show("CPF :" + mskCpf.Text + "\n" +
+                "CNPJ :" + mskCnpj.Text +"\n" +
+                "CVC :"+ mskCvc.Text +"\n" +
+                "Data de validade :" + dtpValidade.Text + "\n" +
+                "Titular do cartão:"+ txtTitular.Text + "\n" +
+                "Senha:"+txtSenha.Text + "\n" +
+                "Numero do cartão"+mskCartao.Text );
+
+                
+                
+
+               
+            
+        }
+
+        private void mskCvc_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+            string email = mskCvc.Text;
+            if ("" != mskCvc.Text) 
+            {
+                mskCvc.Focus();
+                MessageBox.Show("quantidade de numero insuficiente");
+                mskCvc.BackColor = Color.Red;
+                return;
+            }
+        }
+
+        private void mskCpf_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+            string CPF = mskCpf.Text;  
+            if(""== mskCpf.Text)
+            {
+                mskCpf.Focus();
+                MessageBox.Show("digite a quantidade certa de caracteres");
+                mskCpf.BackColor = Color.Red;
+                return;
+            }
+        }
+
+        private void mskCnpj_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+            string CPF = mskCnpj.Text;
+            if (mskCnpj.Text == "") 
+
+            {
+                mskCnpj.Focus();
+                MessageBox.Show("digite a quantidade certa de caracteres");
+                mskCpf.BackColor = Color.Red;
+                return;
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            rbCnpj.Checked= false;  
+            rbCpf.Checked= false;
+            mskCvc.Text = "";
+            txtSenha.Text = "";
+            txtTitular.Text = "";
+            mskCartao.Text = "";
+            mskCpf.Text = "";
+            mskCnpj.Text = "";
+            dtpValidade.Text = "";
+            }
+
+        private void pbOlho1_Click(object sender, EventArgs e)
+        {
+            if (txtSenha.PasswordChar == '*')
+            {
+                txtSenha.PasswordChar = '\0'; // Mostrar a senha
+                pbOlho1.Image = Image.FromFile(@"..\..\Imagem\olho (2) certo.png");
+            }
+            else
+            {
+                txtSenha.PasswordChar = '*'; // Ocultar a senha
+                pbOlho1.Image = Image.FromFile(@"..\..\Imagem\olho (3) certo.png");
+            }
         }
     }
 }
