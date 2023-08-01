d3482a4fbe88905a46c2528ef4ec9567360e5def
briansammm <briansampaio29@gmail.com>
2023-08-01 19:35:34 -0300

    criando telas

---
+++ Projeto1/Projeto1/frmEsqueceuSenha.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace projeto1
+{
+    partial class frmEsqueceuSenha
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtEmailRecapSenha = new System.Windows.Forms.TextBox();
+            this.btnRedefinir = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(215, 167);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(503, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Informe um email para o desejado";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(227, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(477, 55);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "REDEFINIR SENHA";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(308, 215);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(309, 37);
+            this.label3.TabIndex = 2;
+            this.label3.Text = " redefinir uma senha";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(175, 282);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "EMAIL";
+            // 
+            // txtEmailRecapSenha
+            // 
+            this.txtEmailRecapSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmailRecapSenha.Location = new System.Drawing.Point(180, 323);
+            this.txtEmailRecapSenha.Name = "txtEmailRecapSenha";
+            this.txtEmailRecapSenha.Size = new System.Drawing.Size(406, 31);
+            this.txtEmailRecapSenha.TabIndex = 4;
+            // 
+            // btnRedefinir
+            // 
+            this.btnRedefinir.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnRedefinir.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRedefinir.ForeColor = System.Drawing.Color.Red;
+            this.btnRedefinir.Location = new System.Drawing.Point(222, 417);
+            this.btnRedefinir.Name = "btnRedefinir";
+            this.btnRedefinir.Size = new System.Drawing.Size(450, 46);
+            this.btnRedefinir.TabIndex = 5;
+            this.btnRedefinir.Text = "REDEFINIR SENHA";
+            this.btnRedefinir.UseVisualStyleBackColor = false;
+            this.btnRedefinir.Click += new System.EventHandler(this.btnRedefinir_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(419, 469);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(253, 27);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "VOLTAR AO LOGIN";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(-28, -5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(159, 173);
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
+            // frmEsqueceuSenha
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCoral;
+            this.ClientSize = new System.Drawing.Size(928, 597);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnRedefinir);
+            this.Controls.Add(this.txtEmailRecapSenha);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmEsqueceuSenha";
+            this.Text = "ESQUECEU A SENHA?";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtEmailRecapSenha;
+        private System.Windows.Forms.Button btnRedefinir;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ Projeto1/Projeto1/frmEsqueceuSenha.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace projeto1
+{
+    public partial class frmEsqueceuSenha : Form
+    {
+        public frmEsqueceuSenha()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form formVoltar = new frmLogin();
+            formVoltar.Show();
+        }
+
+        bool IsValidEmail(string email)
+        {
+            string pattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+            return System.Text.RegularExpressions.Regex.IsMatch(email, pattern);
+        }
+
+        private void btnRedefinir_Click(object sender, EventArgs e)
+        {
+            string email = txtEmailRecapSenha.Text;
+
+            if (IsValidEmail(email))
+            {
+                MessageBox.Show("Um Email foi enviado para o destinatario para fazer a alterção de senha");
+            }
+            else
+            {
+                MessageBox.Show("Endereço de e-mail inválido!");
+            }
+        }
+    }
+}
