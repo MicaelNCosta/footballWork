567affa5ed88e95d3d8d821d05514bff3d0c3670
PauloVMBastos <paulovictormbastos@gmail.com>
2023-08-01 20:12:11 -0300

    removendo botao

--- projeto1/projeto1/FrmPrincipal.Designer.cs
+++ projeto1/projeto1/FrmPrincipal.Designer.cs
@@ -40,7 +40,6 @@ namespace projeto1
             this.button1.TabIndex = 0;
             this.button1.Text = "Formulario";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // FrmPrincipal
             // 
