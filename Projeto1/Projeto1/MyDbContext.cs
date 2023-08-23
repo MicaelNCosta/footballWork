876586a61083036166d37a34677e77d80e9c7ddd
MicaelNCosta <micael.nc01@gmail.com>
2023-08-23 20:12:56 -0300

    inserindo using necessarios

--- Projeto1/Projeto1/MyDbContext.cs
+++ Projeto1/Projeto1/MyDbContext.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using MySql.Data.MySqlClient;
 
 namespace projeto1
 {
-    class MyDbContext
+    class MyDbContext:DbContext
     {
+      public MyDbContext() : base("name=MyDbContext") { }
+
     }
 }
